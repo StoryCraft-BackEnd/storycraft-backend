222eced57d0a6d78ab04031ccb8aea13dcaba802
YeryungCho <yeryung1128@gmail.com>
2025-04-10 01:22:30 +0900

    초기 개발 환경

---
+++ src/test/java/com/storycraft/storycraft_backend/StorycraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storycraft.storycraft_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorycraftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
