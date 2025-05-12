46e4614d07f028ab4a111135cb00708e98b46926
hyojun <lhj010531@naver.com>
2025-05-13 00:58:26 +0900

    Refactor : 필요 없어진 import 삭제 (BaseTimeEntity 추가로 인한)

--- src/main/java/com/storycraft/story/service/StoryService.java
+++ src/main/java/com/storycraft/story/service/StoryService.java
@@ -8,7 +8,6 @@ import com.storycraft.story.repository.StoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
