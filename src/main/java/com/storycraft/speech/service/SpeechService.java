c0a916d6f780eb5fa87a779456b0d6919be55648
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-06-19 10:32:50 +0900

    Feat: STT 생성 기능 Whisper 연동

--- src/main/java/com/storycraft/speech/service/SpeechService.java
+++ src/main/java/com/storycraft/speech/service/SpeechService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
