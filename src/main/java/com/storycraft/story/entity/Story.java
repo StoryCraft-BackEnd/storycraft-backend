33cce632bc915d499f98a51879f0b4c1bfbcfe0a
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-13 17:45:39 +0900

    Refactor: child entity 도입으로 인한 수정

--- src/main/java/com/storycraft/story/entity/Story.java
+++ src/main/java/com/storycraft/story/entity/Story.java
@@ -21,8 +21,9 @@ public class Story extends BaseTimeEntity {
     @Column(name = "story_id")
     private Long storyId;
 
-    @Column(name = "child_id", nullable = false)
-    private String childId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "child_id", nullable = false)
+    private ChildProfile childId;
 
     @Column(name = "title", nullable = false)
     private String title;
