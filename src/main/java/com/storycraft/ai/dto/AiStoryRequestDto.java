fb150bae2e030670c682a0c7fc672833739f9ace
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-13 19:25:28 +0900

    Refactor: childProfile 도입으로 인한 수정

--- src/main/java/com/storycraft/ai/dto/AiStoryRequestDto.java
+++ src/main/java/com/storycraft/ai/dto/AiStoryRequestDto.java
@@ -1,5 +1,6 @@
 package com.storycraft.ai.dto;
 
+import com.storycraft.profile.entity.ChildProfile;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
