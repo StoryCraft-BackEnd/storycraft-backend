47eaed51e0f6ff1bbc23e37b0698b43a01c9b990
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-15 01:20:32 +0900

    fix: basePackages에 @EntityScan, @EnableJpaRepositories, @ComponentScan 추가

--- src/main/java/com/storycraft/StorycraftBackendApplication.java
+++ src/main/java/com/storycraft/StorycraftBackendApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.storycraft")
+@EnableJpaRepositories(basePackages = "com.storycraft")
+@ComponentScan(basePackages = "com.storycraft")
 public class StorycraftBackendApplication {
 
 	public static void main(String[] args) {
