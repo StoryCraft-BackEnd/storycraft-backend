2d44160a58b443e6d084169ff41ccaafda89b835
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-14 22:12:02 +0900

    Refactor: ChildProfile Entity 도입으로 인한 수정

--- src/main/java/com/storycraft/quiz/repository/QuizCreateRepository.java
+++ src/main/java/com/storycraft/quiz/repository/QuizCreateRepository.java
@@ -2,6 +2,7 @@ package com.storycraft.quiz.repository;
 
 import com.storycraft.quiz.entity.QuizCreate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
