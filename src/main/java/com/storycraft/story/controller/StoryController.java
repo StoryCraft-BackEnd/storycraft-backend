1b905420b74d1ff76631045a534204b6db76c006
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-15 01:14:43 +0900

    Feat: Swagger에서 JWT 토큰 인증 허용

--- src/main/java/com/storycraft/story/controller/StoryController.java
+++ src/main/java/com/storycraft/story/controller/StoryController.java
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/stories")
 @RequiredArgsConstructor
