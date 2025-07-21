d9065ae283b90f556f64c7219d7d95ab799a3c31
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-22 01:42:33 +0900

    fix: prompt 삭제

--- src/main/java/com/storycraft/illustration/dto/IllustrationRequestDto.java
+++ src/main/java/com/storycraft/illustration/dto/IllustrationRequestDto.java
@@ -10,7 +10,4 @@ public class IllustrationRequestDto {
 
     @Schema(description = "대상 동화 ID", example = "1")
     private Long storyId;
-
-    @Schema(description = "동화 내용에 따른 이미지 생성 키워드", example = "숲속에서 모험중인 꼬마용사와 강아지 친구")
-    private String prompt;
 }
