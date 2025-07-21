766ff710abf313e3e6fb1306690845b01f68e4e0
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-22 01:14:05 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/com/storycraft/story/service/StoryService.java
+++ src/main/java/com/storycraft/story/service/StoryService.java
@@ -12,7 +12,6 @@ import com.storycraft.story.repository.StoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
