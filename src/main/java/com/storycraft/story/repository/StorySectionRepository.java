0145b735f97c49ec75d8f50d113d918d0c1ba27d
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-22 01:05:37 +0900

    fix: 동화 수정 시 기존 단락 삭제 및 트랜잭션 처리 오류 수정

--- src/main/java/com/storycraft/story/repository/StorySectionRepository.java
+++ src/main/java/com/storycraft/story/repository/StorySectionRepository.java
@@ -3,6 +3,10 @@ package com.storycraft.story.repository;
 import com.storycraft.story.entity.Story;
 import com.storycraft.story.entity.StorySection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
