57620a45d7941f302bb7a5f4b0e9f95f3e11d1bb
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-22 01:11:52 +0900

    fix: 동화 수정을 키워드만 받아서 새로 생성하게끔 수정

--- src/main/java/com/storycraft/story/dto/StoryUpdateDto.java
+++ src/main/java/com/storycraft/story/dto/StoryUpdateDto.java
@@ -11,9 +11,6 @@ import java.util.List;
 @Schema(description = "동화 수정 요청 DTO")
 public class StoryUpdateDto {
 
-    @Schema(description = "수정할 동화 제목", example = "꼬마 용사와 동물 친구들의 모험")
-    private String title;
-
     @Schema(description = "수정할 키워드 목록", example = "[\"고양이\", \"숲\", \"우정\"]")
     private List<String> keywords;
 }
