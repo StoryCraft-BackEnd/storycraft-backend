130a4b123aac5247d201fe070a84c1424daeb2e6
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-28 00:59:00 +0900

    feat: Swagger에서 테스트 용이 위한 Schema 어노테이션 추가

--- src/main/java/com/storycraft/auth/dto/SignupRequest.java
+++ src/main/java/com/storycraft/auth/dto/SignupRequest.java
@@ -24,6 +24,7 @@ public class SignupRequest {
     @NotBlank(message = "이름은 필수입니다.")
     private String name;
 
+    @Schema(description = "사용자 닉네임", example = "testNickName", required = true)
     @NotBlank(message = "닉네임은 필수입니다.")
     private String nickname;
 
