c454ee587366acdee705d4b636acd2492e51aadf
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-28 01:42:29 +0900

    feat: TTS 생성시 성우 선택, 속도 조절, 언어 선택 기능 추가 및 Polly 연동

--- src/main/java/com/storycraft/speech/dto/TtsCreateRequestDto.java
+++ src/main/java/com/storycraft/speech/dto/TtsCreateRequestDto.java
@@ -13,4 +13,13 @@ public class TtsCreateRequestDto {
 
     @Schema(description = "읽을 동화 ID", example = "1")
     private Long storyId;
+
+    @Schema(description = "읽을 단락 ID", example = "1")
+    private Long sectionId;
+
+    @Schema(description = "성우", example = "Seoyeon")
+    private String voiceId;
+
+    @Schema(description = "TTS 속도", example = "0.8")
+    private float speechRate;
 }
