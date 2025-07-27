6d2e256d1b4c8a014e0dc49f8469a60ab212e3cd
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-28 01:42:45 +0900

    feat: TTS 생성시 성우 선택, 속도 조절, 언어 선택 기능 추가 및 Polly 연동

--- src/main/java/com/storycraft/speech/dto/TtsCreateRequestDto.java
+++ src/main/java/com/storycraft/speech/dto/TtsCreateRequestDto.java
@@ -4,6 +4,7 @@ import com.storycraft.story.entity.Story;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
