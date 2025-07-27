c9440c7e4c5f19e0881bcb5269bf9b42db861629
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-28 00:58:45 +0900

    feat: Swagger에서 테스트 용이 위한 Schema 어노테이션 추가

--- src/main/java/com/storycraft/auth/dto/SignupRequest.java
+++ src/main/java/com/storycraft/auth/dto/SignupRequest.java
@@ -15,6 +15,7 @@ public class SignupRequest {
     @NotBlank(message = "이메일은 필수입니다.")
     private String email;
 
+    @Schema(description = "사용자 비밀번호 (8자 이상)", example = "test1234", required = true)
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
