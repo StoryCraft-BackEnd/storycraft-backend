16288352daf93a990e2a5657605df32dcf64ba5d
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-07-28 01:00:44 +0900

    feat: Swagger에서 테스트 용이 위한 Schema 어노테이션 추가

--- src/main/java/com/storycraft/auth/dto/LoginRequestDto.java
+++ src/main/java/com/storycraft/auth/dto/LoginRequestDto.java
@@ -7,5 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginRequestDto {
     private String email;
+
+    @Schema(description = "사용자 비밀번호 (8자 이상)", example = "test1234")
     private String password;
 }
