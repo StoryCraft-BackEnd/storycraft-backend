b1c55fbc1f1bd068712e7d9e72572cc5a68e5c31
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-08-05 23:09:43 +0900

    feat: 동화 조회시 동화 읽음 여부, 학습 시간등 추가

--- src/main/java/com/storycraft/story/controller/StoryController.java
+++ src/main/java/com/storycraft/story/controller/StoryController.java
@@ -96,6 +96,13 @@ public class StoryController {
             @Parameter(description = "자녀 프로필 ID", example = "1")
             @RequestParam(name = "childId") Long childId
     ) {
+        StoryResponseDto storyDto = storyService.getStory(id);
+        Optional<StoryProgress> progressOpt = storyProgressService.findByStoryIdAndChildId(id, childId);
+
+        if (progressOpt.isPresent()) {
+            storyDto.setProgress(StoryProgressResponseDto.fromEntity(progressOpt.get()));
+        }
+
         return ResponseEntity.ok(
                 new ApiResponseDto<>(200, "동화 조회에 성공했습니다.", storyService.getStory(id))
         );
