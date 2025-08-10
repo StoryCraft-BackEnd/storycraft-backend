6e92f71777c0f0140d0497c58bf960df7a434750
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-08-10 13:28:37 +0900

    feat: 동화 생성시 중요 단어 추출 기능 추가

--- src/main/java/com/storycraft/dictionary/service/AiDictionaryService.java
+++ src/main/java/com/storycraft/dictionary/service/AiDictionaryService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
