db0d3f48b32bd70f50b771a0ec1146fabab54a40
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-08-10 13:24:07 +0900

    feat: 동화 생성시 중요 단어 추출 기능 추가

--- src/main/java/com/storycraft/dictionary/controller/DictionaryController.java
+++ src/main/java/com/storycraft/dictionary/controller/DictionaryController.java
@@ -16,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/dictionaries")
 @RequiredArgsConstructor
