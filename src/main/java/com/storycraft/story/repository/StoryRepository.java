2f35d0d2ae1ac24102b29a6e13a9c8b0975eb12e
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-08-12 19:41:23 +0900

    refactor: 동화 생성 API childId 파라미터 구조 개선 및 OwnershipGuard 적용

--- src/main/java/com/storycraft/story/repository/StoryRepository.java
+++ src/main/java/com/storycraft/story/repository/StoryRepository.java
@@ -5,6 +5,7 @@ import com.storycraft.story.entity.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StoryRepository extends JpaRepository<Story, Long> {
     List<Story> findAllByChildId(ChildProfile childId);
