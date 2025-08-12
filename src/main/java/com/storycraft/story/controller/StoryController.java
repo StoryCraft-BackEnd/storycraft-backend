1838d81d97bc747ed112570e8177d1e8e9a257cd
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-08-12 19:47:36 +0900

    refactor: 동화 생성 API childId 파라미터 구조 개선 및 OwnershipGuard 적용

--- src/main/java/com/storycraft/story/controller/StoryController.java
+++ src/main/java/com/storycraft/story/controller/StoryController.java
@@ -23,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import com.storycraft.global.security.OwnershipGuard;
 
 import java.util.Optional;
 
