89d4949fb5434af0d54faee020af1c8d308471e1
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-09-08 20:28:59 +0900

    fix: 파라미터 이름 수정

--- src/main/java/com/storycraft/story/controller/StoryController.java
+++ src/main/java/com/storycraft/story/controller/StoryController.java
@@ -168,7 +168,7 @@ public class StoryController {
         ChildProfile child = ownershipGuard.getOwnedChildOrThrow(childId, userId);
 
         return ResponseEntity.ok(
-                new ApiResponseDto<>(200, "동화 수정에 성공했습니다.", storyService.updateStory(id, child, dto))
+                new ApiResponseDto<>(200, "동화 수정에 성공했습니다.", storyService.updateStory(storyId, child, dto))
         );
     }
 
