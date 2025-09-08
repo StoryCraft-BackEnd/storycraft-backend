b13dc1e10ae1b53c2e89312c2c186a6e3305a97f
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-09-08 20:29:10 +0900

    fix: 파라미터 이름 수정

--- src/main/java/com/storycraft/story/controller/StoryController.java
+++ src/main/java/com/storycraft/story/controller/StoryController.java
@@ -187,7 +187,7 @@ public class StoryController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteStory(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @Parameter(description = "삭제할 동화 ID", example = "1") @PathVariable(name = "id") Long id,
+            @Parameter(description = "삭제할 동화 ID", example = "1") @PathVariable(name = "storyId") Long storyId,
             @Parameter(description = "자녀 프로필 ID", example = "1") @RequestParam(name = "childId") Long childId
     ) {
         Long userId = userDetails.getUser().getId();
